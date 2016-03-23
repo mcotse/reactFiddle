26a3a671f93f87800a4e0edb5aba8ae57fca7a81
Matthew Tse <mtse9779@gmail.com>
2016-03-23 01:07:04 -0400

    both props and state

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -4,7 +4,9 @@ import React from "react";
 export default class Title extends React.Component {
   render() {
     return (
-      <h1>{this.props.title}</h1>
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
     );
   }
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -5,12 +5,23 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
   render() {
+    setTimeout(() => {
+      this.setState({title: "Welcome Matt"})
+    },2000);
+
     const title = "Welcome Matt";
 
     return (
       <div>
-        <Header title = {title}/>
+        <Header title = {this.state.title}/>
+        <Header title = {"Other title"} />
         <Footer />
       </div>
     );
