213d9c9a7beec10890fc685c461dc2f902f9f7a0
Matthew Tse <mtse9779@gmail.com>
2016-03-23 22:16:25 -0400

    added menu

---
+++ src/js/components/Header/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>Featured</a>
+        <a>Archives</a>
+        <a>Settings</a>
+      </div>
+    );
+  }
+}
