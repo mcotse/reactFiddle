91cfb8613bdb22cf61e29c36b2aebf10120b873d
Matthew Tse <mtse9779@gmail.com>
2016-03-23 23:09:11 -0400

    menu styling

--- src/js/components/Header/Menu.js
+++ src/js/components/Header/Menu.js
@@ -5,9 +5,9 @@ export default class Title extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" class="btn btn-primary">Featured</button>
-        <button type="button" class="btn btn-success">Archives</button>
-        <button type="button" class="btn btn-danger">Settings</button>
+        <button type="button" class="btn btn-default">Featured</button>
+        <button type="button" class="btn btn-primary">Archives</button>
+        <button type="button" class="btn btn-success">Settings</button>
       </div>
     );
   }
