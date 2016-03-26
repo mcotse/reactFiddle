7f1f1abcac714a545ae4c1ee90155b3445f63a8c
Matthew Tse <mtse9779@gmail.com>
2016-03-26 16:47:37 -0400

    unction to set collapsed state

--- src/js/components/Header/Navbar.js
+++ src/js/components/Header/Navbar.js
@@ -3,29 +3,51 @@ import { Link } from "react-router";
 
 
 export default class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      collapsed:true,
+    };
+  }
+
+  toggleCollapse() {
+    const collapsed = !this.state.collapsed;
+    this.setState({collapsed});
+  }
+
   render() {
+    const { location } = this.props;
+    const { collapsed } = this.state;
+    const featuredClass = location.pathname === "/" ? "active" : "";
+    const archivesClass = location.pathname.match(/^\/archives/) ? "active" : "";
+    const settingsClass = location.pathname.match(/^\/settings/) ? "active" : "";
+    const navClass = collapsed ? "collapse" : "";
+
+
     return (
       <div>
-        <div class="navbar navbar-default" role="navigation">
-          <div class="containter">
-            <div clas="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <Link to="/" class="navbar-brand">ReactFiddle</Link>
+              <a class="navbar-brand" href="#">ReactFiddle</a>
             </div>
-            <div id="navbar" class="collapse navbar-collapse" aria-expanded ="false">
+
+            <div class="collapse navbar-collapse" id="navbar">
               <ul class="nav navbar-nav">
-                <li><Link to="about">About</Link></li>
-                <li><Link to="App">App</Link></li>
-                <li><Link to="Settings">Settings</Link></li>
+                <li><Link activeClassName="active" to="about">About</Link></li>
+                <li><Link activeClassName="active" to="App">App</Link></li>
+                <li><Link activeClassName="active" to="Settings">Settings</Link></li>
               </ul>
+
             </div>
           </div>
-        </div>
+        </nav>
         {/*}<Link to="about" type="button" class="btn btn-default">About</Link>
         <Link to="app" type="button" class="btn btn-primary">App</Link>
         <Link to="settings" type="button" class="btn btn-success">Settings</Link>*/}
