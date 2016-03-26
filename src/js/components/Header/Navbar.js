fcaee7cf9be135b253c21f396cffd55a5e5ae13a
Matthew Tse <mtse9779@gmail.com>
2016-03-25 22:58:50 -0400

    navbar items

--- src/js/components/Header/Navbar.js
+++ src/js/components/Header/Navbar.js
@@ -6,16 +6,23 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-default navbar-static-top">
-          <div class="containter">
+        <nav class="navbar navbar-default">
+          <div class="containter-fluid">
             <div clas="navbar-header">
               <Link to="/" class="navbar-brand">ReactFiddle</Link>
             </div>
+            <div id="navbar" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li><Link to="about">About</Link></li>
+                <li><Link to="App">App</Link></li>
+                <li><Link to="Settings">Settings</Link></li>
+              </ul>
+            </div>
           </div>
         </nav>
-        <Link to="about" type="button" class="btn btn-default">About</Link>
+        {/*}<Link to="about" type="button" class="btn btn-default">About</Link>
         <Link to="app" type="button" class="btn btn-primary">App</Link>
-        <Link to="settings" type="button" class="btn btn-success">Settings</Link>
+        <Link to="settings" type="button" class="btn btn-success">Settings</Link>*/}
       </div>
     );
   }
