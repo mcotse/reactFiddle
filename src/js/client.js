ead61d880dde06dd20cf5c477c9bca19365c7c45
Matthew Tse <mtse9779@gmail.com>
2016-03-25 21:29:55 -0400

    import new pages

--- src/js/client.js
+++ src/js/client.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import {Router, Route, IndexRoute, hashHistory} from "react-router"
 
 import Layout from "./components/Layout";
+import About from "./pages/About";
+import App from "./pages/App";
+import Settings from "./pages/Settings";
 
 
 const app = document.getElementById('app');
