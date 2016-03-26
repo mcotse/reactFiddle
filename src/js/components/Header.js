82b00738c8b1466478928a27282e667d745daac2
Matthew Tse <mtse9779@gmail.com>
2016-03-25 22:31:23 -0400

    removed title and switched to paper bootstrap theme

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Title from "./Header/Title";
 import Menu from "./Header/Menu"
 export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <Title/>
         <Menu/><br></br>
       </div>
     );
--- src/js/components/Header/Menu.js
+++ src/js/components/Header/Menu.js
@@ -6,6 +6,13 @@ export default class Title extends React.Component {
   render() {
     return (
       <div>
+        <nav class="navbar navbar-default navbar-static-top">
+          <div class="containter">
+            <div clas="navbar-header">
+              Hello
+            </div>
+          </div>
+        </nav>
         <Link to="about" type="button" class="btn btn-default">About</Link>
         <Link to="app" type="button" class="btn btn-primary">App</Link>
         <Link to="settings" type="button" class="btn btn-success">Settings</Link>
