9e7e2a8a147905039481108dee109a949e327327
Matthew Tse <mtse9779@gmail.com>
2016-03-25 21:54:45 -0400

    removed redundent function dependency handlechange/click

--- src/js/pages/App.js
+++ src/js/pages/App.js
@@ -1,10 +1,54 @@
 import React from "react";
 
+
 export default class App extends React.Component {
-  render(){
-    return(
+
+
+  constructor(){
+    super();
+    this.state = {
+      content: "Type something below!"
+    };
+  }
+  //handle content changes
+  handleChange(e){
+    const content = e.target.value;
+    this.setState({content});
+  }
+
+  handleClick(){
+    this.setState({content: ""});
+    this.TextInput.value ="";
+
+  }
+  //
+  // //change the content state
+  // changeContent(content){
+  //   this.setState({content});
+  // }
+  // //clear the content state
+  // clearContent(){
+  //   this.setState({content: ""});
+  // }
+
+  render() {
+    return (
       <div>
-        <h1>App</h1>
+        {/*display content state thats being passed in*/}
+        <div><h3>{this.props.content}</h3></div>
+        <input
+          defaultValue = "Change Me"
+          // value = {this.props.content}
+          onChange={this.handleChange.bind(this)}
+          ref = {(ref) => this.TextInput = ref}
+        />
+        <br/>
+        <input
+          class="btn btn-xs btn-danger"
+          type="button"
+          value="Clear"
+          onClick={this.handleClick.bind(this)}
+        />
       </div>
     );
   }
