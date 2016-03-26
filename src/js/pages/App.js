9461386f1034def4458af62b8c89ed0a0d1ea491
Matthew Tse <mtse9779@gmail.com>
2016-03-25 21:30:40 -0400

    semicolon after import

--- src/js/pages/App.js
+++ src/js/pages/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header.js"
-import Footer from "../components/Footer.js"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default class App extends React.Component {
   render(){
